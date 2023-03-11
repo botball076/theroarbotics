7240d1055c3830db626362040a020a56ae2be0ab
botball076 <102452299+botball076@users.noreply.github.com>
2023-03-11 11:38:19 -0800

    - Added Code for Wombat

--- main (2).c
+++ main (2).c
@@ -7,18 +7,54 @@ int main()
     int left_motor = 1;
     int right_motor = 0;
     int analog_sensor = 5;
+    int servo = 0;
     
- 	//when robot is on white//
-    while (analog(analog_sensor)<2000)
-    {
-      //turn right for red poms//
-        motor (left_motor,100);
-        motor (right_motor,0);
+    //ping pong mission/
+    
+ 	//when robot is on white
+    
+    enable_servo(servo);
+    
+     //forward
+    motor(left_motor,100);
+    motor(right_motor,100);
+    msleep(1150);
+    
+    //turning right
+    motor (left_motor,200);
+    motor (right_motor,0);
+    msleep (1600);
+    
+    //stop
+    motor (left_motor,0);
+    motor (right_motor,0);
+    msleep (1000);
+    
+    cmpc(right_motor);
         
-    if //robot isnt on white line do this//
-    {
-        
-    }
-    }
-    return 0;
-}
+    //line follow
+    //put when distance sensor sees the white 
+
+    while(gmpc(right_motor) < 6000)
+    { 
+        printf("hfjdklbiqhrg");
+        //tophat senses black line
+       if (analog(analog_sensor) > 1500)
+       {
+           //turn right
+           motor (left_motor,0);
+           motor (right_motor,100);
+       }
+        //tophat senses white
+        else 
+        {
+            //turn left
+            motor (left_motor,100);
+            motor (right_motor,0);
+        }
+   	 }
+    	//attempt to drag blue cup
+    	
+    	set_servo_position(0,
+  		return 0;
+  }
